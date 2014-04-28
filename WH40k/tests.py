b9a10cd75535e94d6f476e20447903aa63f3c0a0
jhick6671 <jhick6671@ung.edu>
2014-04-28 11:12:01 -0400

    Wound Counter
    This program takes in from the user the number dice being rolled, base number for hits to hit, base number for wounds to wound, and number for saving throws. Following the rules laid out in the Warhammer 40k rule book the program determines which of the dice pass all conditions to be consider a damage to defend unit.

--- WH40k/tests.py
+++ WH40k/tests.py
@@ -4,13 +4,30 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+import random
 
 from django.test import TestCase
+from .WoundCounter import Dice
+random.seed(0)
+class ToHitTest(TestCase):
+    def test_ToHit(self):
+        """ In this test the ToHit part of the WoundCounter is being tested. The outcome should be the number of hits
+        being delivered to the defending unit."""
+        # random.seed(0)
+        d = Dice(20, 4, 4, 3)
+        self.assertEqual(d.ToHit(),13)
 
+class ToWoundTest(TestCase):
+    def test_ToWound(self):
+        """ In this test the ToWound part of the WoundCounter is being tested. The outcome should be the number of wounds
+        being delivered to the defending unit."""
+        # random.seed(0)
+        d = Dice(20, 4, 4, 3)
+        self.assertEqual(d.ToWound(),10)
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class WoundsDealtTest(TestCase):
+    def test_wound_counter(self):
+        """ In this test the whole WoundCounter python script is ran. The outcome is the number of wound that"""
+        # random.seed(0)
+        d = Dice(20, 4, 4, 3)
+        self.assertEqual(d.WoundsDealt(),4)
