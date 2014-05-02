2f0e1ab63ab8b88b2dfddbc85067e862139b5b35
jhick6671 <jhick6671@ung.edu>
2014-05-02 13:34:46 -0400

    The WoundCounter.py is connected to a web page.
    The Woundcounter.py is now working with the webage and displaying the correct information. Basic style has been done, needs pictures adds to finish it up.

--- WH40k/models.py
+++ WH40k/models.py
@@ -4,7 +4,16 @@ class WoundCounter(models.Model):
     dice = models.CharField(max_length=3)
     hit = models.CharField(max_length=3)
     wound = models.CharField(max_length=3)
-    save = models.CharField(max_length=3)
+    saves = models.CharField(max_length=3)
+
+    def __unicode__(self):
+        return "{}-{}-{}-{}".format(self.dice,
+                                    self.hit,
+                                    self.wound,
+                                    self.saves)
+
+    def get_absolute_url(self):
+        return '/woundcounter/results/{}'.format(self.pk)
 
 class Army(models.Model):
     name = models.CharField(max_length=30)
